e376dd69e2882944b2f5970d1b5badcfdd1e1ce1
mrintro <aniketpanwar.1998@gmail.com>
2022-06-16 14:58:39 +0530

    graph created.

--- app/src/main/java/com/example/chartpoc/MainActivity.kt
+++ app/src/main/java/com/example/chartpoc/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.chartpoc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var values : ArrayList<Entry>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpData()
         setUpChart()
+    }
+
+    private fun setUpData() {
+        values = ArrayList<Entry>()
+        values.add(Entry(1f,100f))
+        values.add(Entry(2f,150f))
+        values.add(Entry(3f,300f))
+
     }
 
     private fun setUpChart() {
@@ -26,6 +45,37 @@ class MainActivity : AppCompatActivity() {
 
         var xAxis = chart.xAxis
         var leftAxis = chart.axisLeft
+
+        var lineDataSet : LineDataSet
+
+        if(chart.data != null && chart.data.dataSetCount>0) {
+
+        } else {
+            lineDataSet = LineDataSet(values, "sample")
+            lineDataSet.setDrawIcons(false);
+            lineDataSet.enableDashedLine(10f, 5f, 0f);
+            lineDataSet.enableDashedHighlightLine(10f, 5f, 0f);
+            lineDataSet.color = Color.DKGRAY;
+            lineDataSet.setCircleColor(Color.DKGRAY);
+            lineDataSet.lineWidth = 1f;
+            lineDataSet.circleRadius = 3f;
+            lineDataSet.setDrawCircleHole(false);
+            lineDataSet.valueTextSize = 9f;
+            lineDataSet.setDrawFilled(true);
+            lineDataSet.formLineWidth = 1f;
+            lineDataSet.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            lineDataSet.formSize = 15f
+
+            var dataSets : ArrayList<ILineDataSet> = ArrayList<ILineDataSet>()
+
+            dataSets.add(lineDataSet)
+
+            var data : LineData = LineData(dataSets)
+            chart.data = data
+
+
+        }
+
 
 
     }
