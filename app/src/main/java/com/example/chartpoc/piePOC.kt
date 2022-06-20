c3b40da845153674a4062f236c46c0400e6556db
mrintro <aniketpanwar.1998@gmail.com>
2022-06-20 12:22:51 +0530

    Added Pie Chart

---
+++ app/src/main/java/com/example/chartpoc/piePOC.kt
@@ -0,0 +1,11 @@
+package com.example.chartpoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class piePOC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pie_poc)
+    }
+}
